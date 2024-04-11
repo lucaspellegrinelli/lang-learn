8cd7dc813703768d87a539c66b056f7c3c1a4ff7
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-04-11 18:49:12 -0300

    Added wordpack query

--- assets/js/pocketbase.js
+++ assets/js/pocketbase.js
@@ -1,1 +1,31 @@
 const pb = new PocketBase("https://pb.ellep.dev/");
+
+const authStore = {
+    userEmail: null,
+    updateEmail: function() {
+        this.userEmail = pb.authStore.isValid ? pb.authStore.model.email : null;
+    },
+    login: async function() {
+        await pb.collection("users").authWithOAuth2({ provider: "google" });
+        this.updateEmail();
+    },
+    logout: function() {
+        pb.authStore.clear();
+        this.updateEmail();
+    }
+}
+
+const editorStore = {
+    wordPacks: [],
+    updateWordPacks: async function() {
+        this.wordPacks = await pb.collection('wordpacks').getFullList({ expand: "words_via_pack" });
+    },
+}
+
+document.addEventListener("alpine:init", () => {
+    Alpine.store("auth", authStore);
+    Alpine.store("auth").updateEmail();
+
+    Alpine.store("editor", editorStore);
+    Alpine.store("editor").updateWordPacks();
+});
