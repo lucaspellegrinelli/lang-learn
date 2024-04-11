618adef61d13710670612586451d0c438a1bbe36
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-04-11 15:58:45 -0300

    Extracted dark mode toggle

---
+++ js/common.js
@@ -0,0 +1,6 @@
+const toggleDarkMode = () => {
+    const html = document.querySelector('html');
+    const currentTheme = html.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    html.setAttribute('data-theme', newTheme);
+};
